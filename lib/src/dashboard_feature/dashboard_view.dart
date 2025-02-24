7c4f408992058237645d72f58e42deeee17080bc
agungfir98 <agungfir98@gmail.com>
2025-02-24 12:15:39 +0700

    feat: budget screen

--- lib/src/dashboard_feature/dashboard_view.dart
+++ lib/src/dashboard_feature/dashboard_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:oraboros/src/dashboard_feature/screens/budget/budget_screen.dart';
 import 'package:oraboros/src/dashboard_feature/screens/home/home_screen.dart';
 import 'package:oraboros/src/dashboard_feature/settings/settings_controller.dart';
 import 'package:oraboros/src/dashboard_feature/settings/settings_view.dart';
@@ -40,7 +41,7 @@ class _DashboardViewState extends State<DashboardView> {
           icon: const Icon(Icons.pie_chart_outline_outlined),
           label: "budgets",
         ),
-        "widget": const Text("Budgets")
+        "widget": const BudgetScreen()
       },
       {
         "navItem": BottomNavigationBarItem(
@@ -66,50 +67,53 @@ class _DashboardViewState extends State<DashboardView> {
       },
     ];
 
-    return Scaffold(
-        extendBody: true,
-        bottomNavigationBar: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(50),
-              border: Border.all(
-                color: Theme.of(context).colorScheme.outline,
-                width: 2,
-              ),
-              boxShadow: [
-                BoxShadow(
-                  offset: const Offset(5, 5),
-                  color: Theme.of(context).colorScheme.shadow,
+    return SafeArea(
+      child: Scaffold(
+          extendBody: true,
+          bottomNavigationBar: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(50),
+                border: Border.all(
+                  color: Theme.of(context).colorScheme.outline,
+                  width: 2,
                 ),
-              ]),
-          clipBehavior: Clip.hardEdge,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(50),
-            child: BottomNavigationBar(
-              type: BottomNavigationBarType.shifting,
-              backgroundColor: Theme.of(context).colorScheme.primary,
-              currentIndex: _currentIndex,
-              showUnselectedLabels: false,
-              unselectedItemColor: Colors.black,
-              selectedItemColor: Theme.of(context).colorScheme.secondary,
-              selectedFontSize: 14,
-              onTap: (int index) {
-                setState(() {
-                  _currentIndex = index;
-                });
-                if (kDebugMode) {
-                  print(index);
-                }
-              },
-              items: sections
-                  .map<BottomNavigationBarItem>((section) => section['navItem'])
-                  .toList(),
+                boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(5, 5),
+                    color: Theme.of(context).colorScheme.shadow,
+                  ),
+                ]),
+            clipBehavior: Clip.hardEdge,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: BottomNavigationBar(
+                type: BottomNavigationBarType.shifting,
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                currentIndex: _currentIndex,
+                showUnselectedLabels: false,
+                unselectedItemColor: Colors.black,
+                selectedItemColor: Theme.of(context).colorScheme.secondary,
+                selectedFontSize: 14,
+                onTap: (int index) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                  if (kDebugMode) {
+                    print(index);
+                  }
+                },
+                items: sections
+                    .map<BottomNavigationBarItem>(
+                        (section) => section['navItem'])
+                    .toList(),
+              ),
             ),
           ),
-        ),
-        body: SafeArea(
-          child: sections[_currentIndex]['widget'],
-        ));
+          body: SafeArea(
+            child: sections[_currentIndex]['widget'],
+          )),
+    );
   }
 }
