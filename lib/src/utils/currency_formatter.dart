d20e7d966654c817da8f456bc1b8ffa87639b26e
agungfir98 <agungfir98@gmail.com>
2025-02-24 09:40:45 +0700

    currency input formatter

---
+++ lib/src/utils/currency_formatter.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+
+class CurrencyInputFormatter extends TextInputFormatter {
+  final NumberFormat _formatter = NumberFormat.decimalPattern('en_US');
+
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    // If the new value is empty, return as is
+    if (newValue.text.isEmpty) {
+      return newValue;
+    }
+
+    // Remove all non-digit characters
+    String digitsOnly = newValue.text.replaceAll(RegExp(r'[^\d]'), '');
+
+    // Handle backspace when only a zero is left
+    if (digitsOnly.isEmpty) {
+      return const TextEditingValue(text: '');
+    }
+
+    // Convert to number
+    int number = int.parse(digitsOnly);
+
+    // Format the number
+    final formatted = _formatter.format(number);
+
+    // Return new value with formatted text
+    return TextEditingValue(
+      text: formatted,
+      selection: TextSelection.collapsed(offset: formatted.length),
+    );
+  }
+}
